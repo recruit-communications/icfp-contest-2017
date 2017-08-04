50a1844569e32e6c493adb51ef867a2fdca754e4
kenkoooo <kenkou.n@gmail.com>
2017-08-05 03:04:48 +0900

    コマンドとしては単一のパスが渡るはずなのでそれを反映する

--- sugoi-dealer/src/test/scala/com/kenkoooo/sugoi/SugoiDealerSuite.scala
+++ sugoi-dealer/src/test/scala/com/kenkoooo/sugoi/SugoiDealerSuite.scala
@@ -4,18 +4,6 @@ import org.scalatest.{FunSuite, Matchers}
 
 class SugoiDealerSuite extends FunSuite with Matchers {
   test("run the program successfully") {
-    new AiProgram(Array("cat")).put("a", 10) shouldBe("a", 0)
-  }
-
-  test("timeout execution") {
-    val (out, code) = new AiProgram(Array("sleep", "10")).put("", 2)
-    out shouldBe ""
-    assert(code != 0)
-  }
-
-  test("failed execution") {
-    val (out, code) = new AiProgram(Array("expr", "1", "/", "0")).put("", 2)
-    out shouldBe ""
-    assert(code != 0)
+    new AiProgram("cat").put("a", 10) shouldBe("a", 0)
   }
 }

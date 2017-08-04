a0bbc98b43868e87c29876cf3c045d0f580df1cb
kenkoooo <kenkou.n@gmail.com>
2017-08-05 01:50:11 +0900

    標準入出力で通信するあたりを描いた

--- sugoi-dealer/build.sbt
+++ sugoi-dealer/build.sbt
@@ -3,4 +3,14 @@ name := "sugoi-dealer"
 version := "1.0"
 
 scalaVersion := "2.12.3"
-        
+
+logLevel := Level.Debug
+
+libraryDependencies ++= Seq(
+  "org.apache.logging.log4j" % "log4j-api" % "2.8.2",
+  "org.apache.logging.log4j" % "log4j-core" % "2.8.2",
+  "org.apache.logging.log4j" %% "log4j-api-scala" % "11.0",
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+)
+
+parallelExecution := false
--- sugoi-dealer/src/main/scala/SugoiDealer.scala
+++ sugoi-dealer/src/main/scala/SugoiDealer.scala
@@ -1,5 +1,53 @@
-object SugoiDealer {
+import java.util.concurrent.TimeUnit
+
+import org.apache.logging.log4j.scala.Logging
+
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{Future, _}
+import ExecutionContext.Implicits.global
+import scala.sys.process.{Process, ProcessIO}
+
+object SugoiDealer extends Logging {
   def main(args: Array[String]): Unit = {
-    
+    logger.debug("debug")
+  }
+}
+
+class AiProgram(val command: Array[String], timeoutSeconds: Long) extends Logging {
+
+  /**
+    * give the program the string as the standard input
+    *
+    * @param input a string given to the program via stdin
+    * @return the output string and exit code
+    */
+  def put(input: String): (String, Int) = {
+    val buffer = new ArrayBuffer[String]()
+    val io = new ProcessIO(
+      in => {
+        val writer = new java.io.PrintWriter(in)
+        writer.println(input)
+        writer.close()
+      },
+      out => {
+        val src = scala.io.Source.fromInputStream(out)
+        for (line <- src.getLines()) {
+          buffer.append(line)
+        }
+        src.close()
+      },
+      _.close())
+    val process = Process(command).run(io)
+    val future = Future(blocking(process.exitValue()))
+
+    val result = try {
+      Await.result(future, duration.Duration(timeoutSeconds, TimeUnit.SECONDS))
+    } catch {
+      case e: TimeoutException =>
+        logger.catching(e)
+        process.destroy()
+        process.exitValue()
+    }
+    (buffer.mkString, result)
   }
 }

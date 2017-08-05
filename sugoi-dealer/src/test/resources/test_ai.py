8bc5f7bf75899488205b20ec5fdeeca669cea4c2
kenkoooo <kenkou.n@gmail.com>
2017-08-05 17:53:00 +0900

    できたところまで

--- sugoi-dealer/src/test/resources/test_ai.py
+++ sugoi-dealer/src/test/resources/test_ai.py
@@ -1,5 +1,6 @@
+import json
 import sys
-from logging import DEBUG, getLogger, StreamHandler, Formatter, FileHandler, WARN
+from logging import DEBUG, getLogger, StreamHandler, Formatter, FileHandler
 
 logger = getLogger("logger")
 logger.setLevel(DEBUG)
@@ -8,18 +9,28 @@ handler1 = StreamHandler()
 handler1.setFormatter(Formatter("%(asctime)s %(levelname)8s %(message)s"))
 
 handler2 = FileHandler(filename="test.log")
-handler2.setLevel(WARN)
+handler2.setLevel(DEBUG)
 handler2.setFormatter(Formatter("%(asctime)s %(levelname)8s %(message)s"))
 
 logger.addHandler(handler1)
 logger.addHandler(handler2)
 
 
+def write_obj(obj):
+    d = json.dumps(obj) + "\n"
+    b = d.encode()
+    n = len(b)
+    sys.stdout.buffer.write(str(n).encode())
+    sys.stdout.buffer.write(":".encode())
+    sys.stdout.buffer.write(b)
+    sys.stdout.flush()
+
+
 def test():
-    print("19:{\"me\": \"kenkoooo\"}")
+    write_obj({"me": "kenkoooo"})
     input()
     input()
-    print("10:123456789")
+    write_obj("aaa")
 
 
 if __name__ == '__main__':

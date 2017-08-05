c97665c1a3d69523690e4f04b5ffc874dcd95da1
kenkoooo <kenkou.n@gmail.com>
2017-08-05 21:34:20 +0900

    RECV/SEND も出すようにした

--- sugoi-dealer/src/main/scala/com/kenkoooo/sugoi/SugoiDealer.scala
+++ sugoi-dealer/src/main/scala/com/kenkoooo/sugoi/SugoiDealer.scala
@@ -4,8 +4,8 @@ import java.io.{File, InputStream, InputStreamReader}
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.scala.Logging
+import org.apache.logging.log4j.{LogManager, Logger}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
@@ -14,20 +14,23 @@ import scala.concurrent._
 import scala.concurrent.duration.{Duration, _}
 import scala.language.postfixOps
 
-object SugoiDealer extends Logging {
+trait BattleLogging {
+  val battleLogger: Logger = LogManager.getLogger("BattleLog")
+}
+
+object SugoiDealer extends Logging with BattleLogging {
   val mapper: ObjectMapper = SugoiMapper.mapper
-  val battleLogger = LogManager.getLogger("BattleLog")
 
   def main(args: Array[String]): Unit = {
     logger.info("sugoi-dealer is starting...")
     val filepath = args(0)
     val map = mapper.readValue[LambdaMap](new File(filepath), classOf[LambdaMap])
     logger.info("lambda map loaded")
-    val programs = for (i: Int <- 1 until args.length) yield new PunterProgram(args(i), i)
+    val programs = for (i: Int <- 1 until args.length) yield new PunterProgram(args(i), i, i == 1)
     val futures = setup(programs, map)
 
     val gameState = new GameState(map, programs.length, futures)
-    play(programs, gameState)
+    val deque = play(programs, gameState)
 
     gameState.calcScore().foreach(v => {
       val (punter, score) = v
@@ -60,7 +63,7 @@ object SugoiDealer extends Logging {
     * @param programs  AI programs
     * @param gameState state of the game
     */
-  private def play(programs: Seq[PunterProgram], gameState: GameState): Unit = {
+  private def play(programs: Seq[PunterProgram], gameState: GameState): ArrayBuffer[Move] = {
     val deque = new ArrayBuffer[Move]()
     programs.foreach { p => deque.append(PassMove(Pass(p.punter))) }
 
@@ -99,10 +102,11 @@ object SugoiDealer extends Logging {
     }
 
     for (_ <- 0 until gameState.edgeCount) programs.foreach(p => playOneTurn(p))
+    deque
   }
 }
 
-class PunterProgram(cmd: String, val punter: Int) extends Logging {
+class PunterProgram(cmd: String, val punter: Int, battler: Boolean = false) extends Logging with BattleLogging {
   var state: Object = _
   var penaltyCount = 0
 
@@ -127,6 +131,12 @@ class PunterProgram(cmd: String, val punter: Int) extends Logging {
     }
   }
 
+  private def outputBattleLog(line: String): Unit = if (battler) battleLogger.info(line.trim)
+
+  private def logSend(line: String): Unit = outputBattleLog(s"SEND $line")
+
+  private def logRecv(line: String): Unit = outputBattleLog(s"RECV $line")
+
   /**
     * execute handshake and command
     *
@@ -142,6 +152,7 @@ class PunterProgram(cmd: String, val punter: Int) extends Logging {
       // handshake
       val handshakeFromPunter = reader.next()
 
+      logSend(handshakeFromPunter)
       logger.info(s"handshake from punter: $handshakeFromPunter")
       val name = SugoiMapper.mapper.readValue(handshakeFromPunter, classOf[HandShakeFromPunter]).me
       val handShakeFromServer = SugoiMapper.mapper.writeValueAsString(HandShakeFromServer(name)) + "\n"
@@ -151,6 +162,7 @@ class PunterProgram(cmd: String, val punter: Int) extends Logging {
       os.write(handShakeFromServer.getBytes)
       os.flush()
 
+      logRecv(handShakeFromServer)
       logger.info(s"handshake to punter: ${handShakeFromServer.length}:$handShakeFromServer")
 
       val commandFromServer = command + "\n"
@@ -159,9 +171,11 @@ class PunterProgram(cmd: String, val punter: Int) extends Logging {
       os.write(commandFromServer.getBytes)
       os.flush()
 
+      logRecv(commandFromServer)
       logger.info(s"command to punter: ${commandFromServer.length}:$commandFromServer")
 
       val fromPunter = reader.next()
+      logSend(fromPunter)
       logger.info(s"command from punter: $fromPunter")
       (fromPunter, 0)
     } catch {

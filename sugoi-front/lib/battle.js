8d6759b82ec321a406acc65596cb29147d87b728
mokemoko <mokemoko331@gmail.com>
2017-08-05 14:44:10 +0900

    add rundeck post.

--- sugoi-front/lib/battle.js
+++ sugoi-front/lib/battle.js
@@ -1,23 +1,41 @@
+require('dotenv').config();
+
 const uuid = require('uuid/v4');
 const db = require('./db');
+const http = require('http');
 
 // 対戦を実行
 // id: 対戦ID
 // num: 対戦人数
 // map: マップID
 function exec({id = uuid(), num = 2, map = "sample"} = {}) {
+  let params = {};
   return pick(num).then((cids) => {
-    // TODO: post rundeck
-    return cids;
-  }).then((cids) => {
-    const params = {
+    params = {
       id: id,
       clients: cids,
-      map: map,
+      map: map
     };
-    return db.addBattle(params).then(() => params);
-  }).then((params) => {
+    return postRundeck(params);
+  }).then(() => {
+    return db.addBattle(params);
+  }).then(() => {
     return params
+  });
+}
+
+// Rundeckにジョブ登録
+function postRundeck({id, map, clients}) {
+  const params = [
+    `authtoken=${process.env.RUNDECK_TOKEN}`,
+    `argString=-game_id+${id}+-map_id+${map}+-punter_ids+${clients.join(',')}`
+  ].join('&')
+  const url = `http://52.198.25.234:4440/api/1/job/ea9912d0-da52-4394-883d-d3b7a10a0e3c/run?${params}`
+
+  return new Promise((fulfill, reject) => {
+    http.get(url, (res) => {
+      fulfill(res);
+    });
   });
 }
 

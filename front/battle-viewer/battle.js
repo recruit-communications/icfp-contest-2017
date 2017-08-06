8a415d558718863e2b8233c97e402ce770af8510
roiti46@gmail.com <torii46@r.recruit.co.jp>
2017-08-06 20:15:20 +0900

    リファクタリング。共通部分を切り出してcommonに配置

--- front/battle-viewer/battle.js
+++ front/battle-viewer/battle.js
@@ -12,32 +12,6 @@ let timeSpan = 500;
 
 /* Graph rendering */
 
-const colours =
-  ["#aec7e8",
-    "#ff7f0e",
-    "#f7b6d2",
-    "#1f77b4",
-    "#2ca02c",
-    "#c5b0d5",
-    "#ffbb78",
-    "#98df8a",
-    "#d62728",
-    "#ff9896",
-    "#dbdb8d",
-    "#9467bd",
-    "#8c564b",
-    "#c7c7c7",
-    "#c49c94",
-    "#e377c2",
-    "#7f7f7f",
-    "#bcbd22",
-    "#17becf",
-    "#9edae5"];
-
-function getPunterColour(punter) {
-  return colours[punter % colours.length];
-}
-
 function renderGraph(graph) {
   console.log(graph);
   initCy(graph,
@@ -282,7 +256,7 @@ function handleGo() {
 
 function forwardBattle(logging) {
   if (row >= moves.length) return;
-  
+
   let data = moves[row][col];
   if (data.claim != undefined) {
     updateEdgeOwner(data.claim.punter, data.claim.source, data.claim.target);
@@ -450,20 +424,6 @@ function doVisualize() {
   }
   while (jsons[jsons.length - 1] == "") jsons.pop();
   start();
-}
-
-function initPunterColours() {
-  for (let i = 0; i < colours.length; i++) {
-    $("#punter-colours").append("<div class=\"colours" + i + "\">" +("  " + i).substr(-2).replace(/ /g, "&ensp;") + "&nbsp;</div>");
-    $(".colours" + i).css({"background-color":colours[i], "display":"inline"});
-  }
-}
-
-
-function updateWidth() {
-  width = $("#line-width").val();
-  $("#cy .edge").css("width", width);
-  console.log("updateWidth with" + width);
 }
 
 function updateSpan() {

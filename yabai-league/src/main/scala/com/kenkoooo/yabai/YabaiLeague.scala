5d69dee5ffa6d7fa7cf4aa3d7f6258d10847447b
kenkoooo <kenkou.n@gmail.com>
2017-08-07 05:27:56 +0900

    ugoita

--- yabai-league/src/main/scala/com/kenkoooo/yabai/YabaiLeague.scala
+++ yabai-league/src/main/scala/com/kenkoooo/yabai/YabaiLeague.scala
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.apache.logging.log4j.scala.Logging
 
+import scala.collection.mutable
 import scala.util.Random
 
 object YabaiSelector extends Logging {
@@ -20,15 +21,35 @@ object YabaiSelector extends Logging {
     val maps = mapper.readValue[List[MapEntry]](YabaiUrl.get(YabaiUrl.mapList), new TypeReference[List[MapEntry]] {})
     val gameList = mapper.readValue[List[GameResult]](YabaiUrl.get(YabaiUrl.gameLog), new TypeReference[List[GameResult]] {})
 
-    gameList.foreach(gameResult => {
-      Option(gameResult.results) match {
-        case Some(results) => {}
-        case _ => {
+    val mapIds = (for (map <- maps) yield map.id).toSet
+    val mapMember = new mutable.TreeMap[PunterId, Int]()
+    maps.foreach(map => mapMember += (map.id -> map.punterNum))
 
+    val punterCount = new mutable.TreeMap[PunterId, Int]().withDefaultValue(0)
+    val punterScore = new mutable.TreeMap[PunterId, Double]().withDefaultValue(0.0)
+
+    gameList.foreach(gameResult => {
+      val mapId = gameResult.map
+      if (mapIds.contains(mapId)) Option(gameResult.results).foreach { results =>
+        val member = results.length
+        if (member > 1) results.zipWithIndex.foreach { case (result, i) =>
+          val punterId = result.punter
+          val point = 1.0 - i.toDouble / (member.toDouble - 1.0)
+
+          punterScore(punterId) += point
+          punterCount(punterId) += 1
         }
       }
     })
+
+    val x = for (entry <- punterIds if punterCount(entry.id) > 0) yield (entry.id, punterScore(entry.id) / punterCount(entry.id), punterCount(entry.id))
+    x.sortBy { case (_, score, _) => score }.reverse.foreach {
+      case (punterId, score, count) =>
+        logger.info(s"${mapper.writeValueAsString(Data(punterId, score, count))}")
+    }
   }
+
+  case class Data(punterId: PunterId, score: Double, count: Int)
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   case class GameResult(@JsonProperty("map_id") map: LambdaMapId,
@@ -48,7 +69,7 @@ object YabaiSelector extends Logging {
   case class Job(url: String, status: String)
 
   @JsonIgnoreProperties(ignoreUnknown = true)
-  case class MapEntry(info: MapInfo, @JsonProperty("created_at") createdAt: Long, id: String, @JsonProperty("punter_num") punterNum: Int, url: String)
+  case class MapEntry(info: MapInfo, @JsonProperty("created_at") createdAt: Long, id: LambdaMapId, @JsonProperty("punter_num") punterNum: Int, url: String)
 
   @JsonIgnoreProperties(ignoreUnknown = true)
   case class MapInfo(rivers: Long, mines: Int, sites: Int)
